557e5e547933b8d15e00a670630dd962152612e1
EthanBlanco15 <ethan.blanco@ucas-edu.net>
2025-02-20 10:46:10 -0700

    viola

--- highscore_leaderboard/main.py
+++ highscore_leaderboard/main.py
@@ -11,4 +11,26 @@
 ####
 
 def hs_leaderboard():
+    print("Welcome to the High Score Leaderboard!\n")
+    highscore_query = int(input("""What would you like to do?
+                1. See the entire leaderboard.
+                2. See the top 10 scores.
+                3. See a specific score via username.
+                4. Exit\n"""))
+    if highscore_query == 1:
+        pass
+    if highscore_query == 2:
+        pass
+    if highscore_query == 3:
+        pass
+    if highscore_query == 4:
+        return print("Thank you for using our system!")
     pass
+
+####
+####
+####
+#Remember to call back this function in MAIN: hs_leaderboard()
+####
+####
+####
