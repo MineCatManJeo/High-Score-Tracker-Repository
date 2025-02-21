30d80f47276db8d1c35cd5f66c4d35ef7c09dccf
EthanBlanco15 <ethan.blanco@ucas-edu.net>
2025-02-21 10:23:22 -0700

    guh

--- highscore_leaderboard/main.py
+++ highscore_leaderboard/main.py
@@ -16,13 +16,18 @@ def hs_leaderboard():
                 1. See the entire leaderboard.
                 2. See the top 10 scores.
                 3. See a specific score via username.
-                4. Exit\n"""))
+                4. Exit.\n"""))
     if highscore_query == 1:
-        pass
+        print("Enter the name of the leaderboard here")
+        return
     if highscore_query == 2:
         pass
     if highscore_query == 3:
-        pass
+        keyw_user = input("Enter a player's username to see all of their registered scores.")
+        if not keyw_user:
+            print("Search failed, please be more specific and try again.")
+            return
+        #Named list of leaderboard
     if highscore_query == 4:
         return print("Thank you for using our system!")
     pass
@@ -30,7 +35,11 @@ def hs_leaderboard():
 ####
 ####
 ####
+####
+####
 #Remember to call back this function in MAIN: hs_leaderboard()
+####
+####
 ####
 ####
 ####
