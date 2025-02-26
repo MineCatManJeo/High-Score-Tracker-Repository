691e3cf35ee654ccf8c64848bf4a14f77e2dc05b
Maximusholdaway11 <max.holdaway@ucas-edu.net>
2025-02-26 19:19:48 +0000

    Need to hurry did some number game code

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -9,8 +9,21 @@ def do_guess(random_num, user_guess):
         return "Lower"
     elif user_guess == random_num:
         return "At"
+    elif user_guess < 1:
+        print("You can't guess a number below 1.")
+        return "None"
+    elif user_guess > 1000:
+        print("You can't guess a number above 1000")
+        return "None"
     
 def get_random_num():
     random_num = random.randint(1, 1000)
     return random_num
 
+def check_guess(user_guess)
+
+def number_guessing_game():
+    random_num = get_random_num()
+    user_number_guess = input("Please type in your guess (the number is between 1 and 1000): ")
+    user_guess = do_guess(random_num, user_number_guess)
+    
