1fd87c1a5f65162e0c20671da4f6afaf61d71fcd
Maximusholdaway11 <max.holdaway@ucas-edu.net>
2025-02-27 05:30:14 +0000

    finished number guessing game

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -20,10 +20,29 @@ def get_random_num():
     random_num = random.randint(1, 1000)
     return random_num
 
-def check_guess(user_guess)
+def check_guess(user_guess, number_of_guesses):
+    if user_guess == "Above":
+        number_of_guesses + 1
+        print("You guessed above the number try again!")
+        return number_of_guesses, False
+    if user_guess == "Below":
+        number_of_guesses + 1
+        print("You guessed below the number try again!")
+        return number_of_guesses, False
+    if user_guess == "At":
+        number_of_guesses + 0
+        print("You guessed the number! Congratulations!")
+        return number_of_guesses, True
+    if user_guess == "None":
+        number_of_guesses + 0
+        return number_of_guesses, False
 
 def number_guessing_game():
+    user_has_guessed_number = False
+    number_of_guesses = 0
     random_num = get_random_num()
-    user_number_guess = input("Please type in your guess (the number is between 1 and 1000): ")
-    user_guess = do_guess(random_num, user_number_guess)
-    
+    while user_has_guessed_number == False:
+        user_number_guess = input("Please type in your guess (the number is between 1 and 1000): ")
+        user_guess = do_guess(random_num, user_number_guess)
+        number_of_guesses, user_has_guessed_number = check_guess(user_guess)
+    return number_of_guesses
