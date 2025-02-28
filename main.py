97de9ce18e76f21f90ab341a588e99a550b20004
maximusholdaway11 <max.holdaway@ucas-edu.net>
2025-02-28 10:51:39 -0700

    figured out the main function more.

--- main.py
+++ main.py
@@ -10,9 +10,24 @@ import user_profiles
 
 import hs_lead
 
+import add_scores
+
+#Adding user scores to a list in a format to be added to the leaderboard
+def add_scores_to_list(high_scores, list_of_scores):
+    list_of_scores.append(high_scores["keyboard"])
+    list_of_scores.append(high_scores["guessing"])
+    list_of_scores.append(high_scores["tic_tac"])
+    return list_of_scores
+
+#Same as above but for games
+def add_games_to_list(high_scores, list_of_games):
+    for key in high_scores:
+        list_of_games.append(key)
+    return list_of_games
+
 #The function that holds all the specific games
-def game_library():
-    high_score_dict = {"keyboard": "Nothin", "guessing": "Nothin", "tic_tac": "Nothin"}
+def game_library(user_key):
+    high_score_dict = {"keyboard": "Nothing", "guessing": "Nothing", "tic_tac": "Nothing"}
     while True:
         user_input = input("""Which game do you want to play?: 
                         1. spacebar presses in a minute (try to press the spacebar as much as possible in a minute)
@@ -22,30 +37,29 @@ def game_library():
         if user_input.isnumeric():
             user_input = int(user_input)
             if user_input == 1:
+                print("Doing this game again WILL OVERWRITE YOUR PREVIOUS SCORE.")
                 keyboard_game_high_score = keyboard_game.spacebar_per_seconds_game()
                 high_score_dict["keyboard"] = keyboard_game_high_score
             elif user_input == 2:
+                print("Doing this game again WILL OVERWRITE YOUR PREVIOUS SCORE.")
                 number_guessing_game_high_score = number_guessing_game.number_guessing_game()
                 high_score_dict["guessing"] = number_guessing_game_high_score
             elif user_input == 3:
+                print("Doing this game again WILL OVERWRITE YOUR PREVIOUS SCORE.")
                 tic_tac_toe_game_high_score = tic_tac_toe_game.tic_tac_toe_game()
                 high_score_dict["tic_tac"] = tic_tac_toe_game_high_score
             elif user_input == 4:
                 print("Hope these games were fun! See you next time.")
                 break
+            else:
+                print("You have not selected one of the options.")
         else:
             print("Please type in a number.")
     return high_score_dict
 
-#Grouping all the account functions into a single one
-def account_management():
-    user_profiles.load_user_profiles()
-    user_profiles.leaderboard_fixer()
-    user_profiles.user_login()
-    user_profiles.save_user_profiles()
-
 #The main user interface
 def main():
+    user_key = user_profiles.user_login()
     while True:
         print("1. Game Library")
         print("2. Account Manager")
@@ -55,9 +69,21 @@ def main():
         if user_input.isnumeric():
             user_input = int(user_input)
             if user_input == 1:
-                game_library()
+                list_of_games = []
+                list_of_scores = []
+                high_scores = game_library(user_key)
+                list_of_games = add_games_to_list(high_scores, list_of_games)
+                list_of_scores = add_scores_to_list(high_scores, list_of_scores)
+                for game in list_of_games:
+                    if game == "keyboard":
+                        add_scores.add_scores_game(user_key, list_of_scores[0], game)
+                    elif game == "guessing":
+                        add_scores.add_scores_game(user_key, list_of_scores[1], game)
+                    elif game == "tic_tac":
+                        add_scores.add_scores_game(user_key, list_of_scores[2], game)
+                        
             elif user_input == 2:
-                account_management()
+                user_key = user_profiles.user_login()
             elif user_input == 3:
                 hs_lead.hs_leaderboard()
             elif user_input == 4:
